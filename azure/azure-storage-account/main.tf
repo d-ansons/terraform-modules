78d00fab2abfac1793c4a74538e8ed84551b7c00
Davis <davis.ansons@outlook.com>
2026-08-05 21:27:25 +0100

    feat(azure-storage-account): implement secure-by-default storage account resource

--- azure/azure-storage-account/main.tf
+++ azure/azure-storage-account/main.tf
@@ -1,1 +1,13 @@
-
+resource "azurerm_storage_account" "this" {
+  name                              = var.name
+  location                          = var.location
+  account_replication_type          = var.account_replication_type
+  account_tier                      = var.account_tier
+  resource_group_name               = var.resource_group_name
+  min_tls_version                   = var.min_tls_version
+  public_network_access_enabled     = var.public_network_access_enabled
+  allow_nested_items_to_be_public   = var.allow_nested_items_to_be_public
+  infrastructure_encryption_enabled = var.infrastructure_encryption_enabled
+  https_traffic_only_enabled        = true
+  tags                              = local.tags
+}
